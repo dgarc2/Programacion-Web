92e2b7e5238fb73cf15a4af61a058e2b6f1f94bb
dgarc2 <daigogarces@hotmail.com>
2024-11-11 07:30:01 -0600

    commit

--- Parcial 3/Templates/index.js
+++ Parcial 3/Templates/index.js
@@ -1,12 +1,21 @@
 const express = require ('express')
+const path = require('path')
 const app = express()
 console.log(__dirname)
 console.log(__filename)
+
+
+app.use(express.json())
+app.use(express.text())
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.json())
 app.get('/administrativos', (req,res)=>{
     console.log(req.query)
     res.send('servidor contestando a peticion')
+    res.render('admin')
 })
 
 app.get('/maestros',(req,res)=>{
