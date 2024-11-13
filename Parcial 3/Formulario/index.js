ae9d177591521eb568031fb8482e56b60d4e8327
dgarc2 <daigogarces@hotmail.com>
2024-11-13 06:48:05 -0600

    commit

---
+++ Parcial 3/Formulario/index.js
@@ -0,0 +1,21 @@
+const express = require ('express')
+const path = require('path')
+const app = express()
+// console.log(__dirname)
+// console.log(__filename)
+
+
+app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({extended:true}))
+
+app.post('/formulario', (req,res)=>{
+    console.log(req.body)
+    res.send(`Hola! ${req.body.nombre}`)
+})
+
+
+
+app.listen(8081,()=>{
+    console.log('servidor escuchando en el puerto 8081')
+})
